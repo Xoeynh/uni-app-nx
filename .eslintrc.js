9fe138c841f8a6a3ad2bbb0c0c7347262cda16ea
xlz122 <122874722@qq.com>
2023-12-29 14:51:23 +0800

    style: 代码注释

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,14 +15,14 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 关闭any类型时的警告
-    '@typescript-eslint/no-explicit-any': 'off',
-    // 组件name ignores数组为忽略（string[]）
+
+    '@typescript-eslint/no-explicit-any': 0,
+    // 组件name
     'vue/multi-word-component-names': ['error', {
       'ignores': []
     }],
 
-    // 禁用var，使用let、const
+    // 禁用var
     'no-var': 2,
     // 禁止在变量定义之前使用
     '@typescript-eslint/no-use-before-define': [
@@ -39,7 +39,7 @@ module.exports = {
     'semi': [2, 'always'],
     // 强制文件末尾换行
     'eol-last': 2,
-    // 箭头函数参数括号(1个参数不需要,1个以上需要)
+    // 箭头函数参数括号(1个参数不需要, 1个以上需要)
     'arrow-parens': [2, 'as-needed'],
     // 箭头函数(箭头前后空格)
     'arrow-spacing': [2, { before: true, after: true }],
@@ -48,6 +48,6 @@ module.exports = {
     // 单行代码最大长度
     'max-len': [2, { code: 120 }],
     // 空格缩进
-    'indent': [2, 2],
+    'indent': [2, 2]
   }
 };
